f4e57d1a467c4d64e85960d2fe49d5ca21fbde13
Shinichi Matsuda <matsu1983@gmail.com>
2016-10-02 13:32:05 +0900

    設定ベタ書き

--- config/initializers/neography.rb
+++ config/initializers/neography.rb
@@ -1,7 +1,7 @@
 
 Neography.configure do |config|
-  config.server         = ENV["GRAPHENEDB_BOLT_SERVER"] || "localhost"
-  config.port           = ENV["GRAPHENEDB_PORT"] || "7474"
+  config.server         = "hobby-cagbfjhcfnhggbkecdimgfnl.dbs.graphenedb.com" || "localhost" # ENV["GRAPHENEDB_BOLT_SERVER"]
+  config.port           = 24789 || "7474" # ENV["GRAPHENEDB_PORT"]
   config.directory      = ""  # prefix this path with '/'
   config.cypher_path    = "/cypher"
   config.gremlin_path   = "/ext/GremlinPlugin/graphdb/execute_script"
@@ -9,7 +9,7 @@ Neography.configure do |config|
   config.log_enabled    = false
   config.max_threads    = 20
   config.authentication = 'basic'  # 'basic' or 'digest'
-  config.username       = ENV["GRAPHENEDB_BOLT_USER"] || 'neo4j'
-  config.password       = ENV["GRAPHENEDB_BOLT_PASSWORD"] || 'password'
+  config.username       = "app57363794-nzRyWS" || 'neo4j' # ENV["GRAPHENEDB_BOLT_USER"]
+  config.password       = "oMzcwUeW340J81SuDN1F" || 'password' # ENV["GRAPHENEDB_BOLT_PASSWORD"]
   config.parser         = MultiJsonParser
 end
